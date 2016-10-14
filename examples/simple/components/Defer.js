8851f4a490a719b3740f2f0e01c39b2b127f11c5
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-10-14 22:15:10 +0200

    Updated simple example with new paths that fail on the client
    This to test the new onError system mainly.

---
+++ examples/simple/components/Defer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { provideHooks } from 'redial';
+
+@provideHooks({
+  defer: ({ setProps, getProps }) => new Promise((resolve) => {
+    const { color } = getProps();
+    if(!color) {
+      if (typeof window === 'object') {
+        throw new Error('Defer: Demo error on client side only');
+      }
+      setTimeout(() => {
+        const getValue = () => Math.round(Math.random() * 255);
+        setProps({color: `rgb(${getValue()}, ${getValue()}, ${getValue()})`});
+        resolve();
+      }, 1000);
+    } else {
+      resolve();
+    }
+  })
+})
+export default class Fetch extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ color: this.props.color }}>Client will get error on blocking</h1>
+      </div>
+    );
+  }
+}
