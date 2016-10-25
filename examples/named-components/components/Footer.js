27a939ebe10292a3a0aa89d9372cf310919f0497
PAkerstrand <patrik.akerstrand@aftonbladet.se>
2016-10-25 17:51:54 +0200

    Uses `redial` in named fragments

--- examples/named-components/components/Footer.js
+++ examples/named-components/components/Footer.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+import { provideHooks } from 'redial';
 
-export default class Footer extends Component {
-  state = {
-    year: new Date().getFullYear(),
-  };
-
+class Footer extends Component {
   render() {
     return (
       <div>
-        <span>Copyright © {this.state.year}</span>
+        <span>Copyright © {this.props.year}</span>
         <span>Made with ❤ by people</span>
         <span>Additional stuff. You know. Because we can.</span>
       </div>
     );
   }
 }
+
+const enhance = provideHooks({
+  fetch: ({ setProps, getProps, force }) => new Promise((resolve) => {
+    const { year } = getProps();
+    if (!year || force) {
+      setTimeout(() => {
+        setProps({
+          year: new Date().getFullYear(),
+        });
+        resolve();
+      }, 250);
+    } else {
+      resolve();
+    }
+  }),
+});
+
+export default enhance(Footer);
