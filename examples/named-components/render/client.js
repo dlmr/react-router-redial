0480da11542feb4da15f6d7975c285cb41b8416b
PAkerstrand <patrik.akerstrand@aftonbladet.se>
2016-10-25 17:18:58 +0200

    Updated rendering to middleware

--- examples/named-components/render/client.js
+++ examples/named-components/render/client.js
@@ -1,24 +1,43 @@
-import { RedialContext } from 'react-router-redial';
+import { useRedial } from 'react-router-redial';
 
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import { Router, browserHistory, applyRouterMiddleware } from 'react-router';
 
 // Render the app client-side to a given container element:
 export default (container, routes) => {
+  const forcePageReloadOnError = true;
+  const goBackOnError = false;
+
+  // Function that can be used as a setting for useRedial
+  function onError(err, { abort, beforeTransition, reason, router }) {
+    if (process.env.NODE_ENV !== 'production') {
+      console.error(reason, err);
+    }
+
+    // We only what to do this if it was a blocking hook that failed
+    if (beforeTransition) {
+      if (forcePageReloadOnError && reason === 'other') {
+          window.location.reload();
+        } else if (goBackOnError && reason !== 'location-changed') {
+          router.goBack();
+        }
+      // Abort current loading automatically
+      abort();
+    }
+  }
+
   const component = (
     <Router
       history={browserHistory}
       routes={routes}
-      render={(props) => (
-        <RedialContext
-          { ...props }
-          blocking={['fetch']}
-          defer={['defer', 'done']}
-          parallel={false}
-          initialLoading={() => <div>Loading…</div>}
-        />
-      )}
+      render={applyRouterMiddleware(useRedial({
+        beforeTransition: ['fetch'],
+        afterTransition: ['defer', 'done'],
+        parallel: true,
+        initialLoading: () => <div>Loading…</div>,
+        onError,
+      }))}
     />
   );
 
--- examples/named-components/render/server.js
+++ examples/named-components/render/server.js
@@ -1,8 +1,7 @@
-import { triggerHooks, RedialContext } from '../../../lib/index';
+import { triggerHooks, useRedial } from '../../../lib/index';
 
-import React from 'react';
 import { renderToString } from 'react-dom/server';
-import { createMemoryHistory, match } from 'react-router';
+import { createMemoryHistory, match, applyRouterMiddleware } from 'react-router';
 
 // Render the app server-side for a given path:
 export default (path, routes) => new Promise((resolve, reject) => {
@@ -12,20 +11,15 @@ export default (path, routes) => new Promise((resolve, reject) => {
   // Match routes based on history object:
   match({ routes, history }, (error, redirectLocation, renderProps) => {
     if (error || !renderProps) {
-      return resolve({
-          html: '',
-          redialProps: null
-      });
+      return reject(error);
     }
 
     // Wait for async data fetching to complete, then render:
-    triggerHooks({
+    return triggerHooks({
       renderProps,
-      hooks: [ 'fetch', 'done' ]
+      hooks: ['fetch', 'done'],
     }).then(({ redialMap, redialProps }) => {
-      const html = renderToString(
-        <RedialContext {...renderProps} redialMap={ redialMap } />
-      );
+      const html = renderToString(applyRouterMiddleware(useRedial({ redialMap }))(renderProps));
       resolve({ html, redialProps });
     })
     .catch(reject);
