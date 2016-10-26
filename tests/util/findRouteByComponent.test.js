5e1d7eefa57af4ade97b6cd228154612c3cccb47
PAkerstrand <patrik.akerstrand@aftonbladet.se>
2016-10-26 09:04:59 +0200

    Adds more tests and better test labels

--- tests/util/findRouteByComponent.test.js
+++ tests/util/findRouteByComponent.test.js
@@ -4,8 +4,19 @@ import findRouteByComponent from '../../src/util/findRouteByComponent';
 
 describe('findRouteByComponent', () => {
   const component = () => {};
-  it('Returns undefined for empty routes', () => {
-    expect(findRouteByComponent(component, [])).toMatch({});
+  it('Returns an empty object for empty routes', () => {
+    expect(findRouteByComponent(component, [])).toEqual({});
+  });
+  it('Returns an empty object when the component cannot be found among the routes', () => {
+    const routes = [
+      {
+        component: function notThisOne() {},
+      },
+      {
+        component: function andNotThisOnceEither() {},
+      },
+    ];
+    expect(findRouteByComponent(component, routes)).toEqual({});
   });
   it('Returns the first matched route', () => {
     const routes = [
@@ -16,6 +27,22 @@ describe('findRouteByComponent', () => {
         component,
       },
     ];
-    expect(findRouteByComponent(component, routes)).toMatch({ route: routes[1] });
+    expect(findRouteByComponent(component, routes)).toEqual({ route: routes[1] });
+  });
+  it('Handles `components` for named routes', () => {
+    const routes = [
+      {
+        component: function ARouteHandler() {},
+      },
+      {
+        components: {
+          a: function AScreen() {},
+          b: function BScreen() {},
+          c: component,
+          d: function DScreen() {},
+        },
+      },
+    ];
+    expect(findRouteByComponent(component, routes)).toEqual({ route: routes[1], name: 'c' });
   });
 });
